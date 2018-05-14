de3a8011c4e297e88ea88c331689e9bb6f65c1b6
David Hulme <dhulme@outlook.com>
2018-05-14 09:53:06 +0100

    Removed old client/server model

--- server/src/api.js
+++
@@ -1,7 +0,0 @@
-const axios = require('axios');
-
-const api = axios.create({
-  baseURL: 'http://localhost:8081'
-});
-
-module.exports = api;
--- server/src/main.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const path = require('path');
-
-const app = express();
-
-app.use(express.static(path.join(__dirname, '../../client/dist')));
-
-app.listen(3000, () => console.log('App listening on port 3000'));
