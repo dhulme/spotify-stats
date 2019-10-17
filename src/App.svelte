cc028806bd373bb4d527388594a4cc094ce827ed
Dave Hulme <dhulme@outlook.com>
2019-10-17 17:10:37 +0100

    Formatting and description

--- src/App.svelte
+++ src/App.svelte
@@ -1,91 +1,95 @@
 <script>
-import { onMount } from 'svelte';
-	import {logIn, initUser, initToken} from './api.js';
-	import SelectPlaylist from './SelectPlaylist.svelte';
-	import TrackFetcher from './TrackFetcher.svelte';
-	import ChartTracks from './ChartTracks.svelte';
-	import ChartArtist from './ChartArtist.svelte';
-	import ChartTopArtists from './ChartTopArtists.svelte';
+  import { onMount } from "svelte";
+  import { logIn, initUser, initToken } from "./api.js";
+  import SelectPlaylist from "./SelectPlaylist.svelte";
+  import TrackFetcher from "./TrackFetcher.svelte";
+  import ChartTracks from "./ChartTracks.svelte";
+  import ChartArtist from "./ChartArtist.svelte";
+  import ChartTopArtists from "./ChartTopArtists.svelte";
 
-	let user;
-	let initUserPromise;
-	let playlistId;
-	let tracks = [];
+  let user;
+  let initUserPromise;
+  let playlistId;
+  let tracks = [];
 
-	onMount(async () => {
-		await initToken();
-		initUserPromise = initUser();
-		user = await initUserPromise;
-	})
+  onMount(async () => {
+    await initToken();
+    initUserPromise = initUser();
+    user = await initUserPromise;
+  });
 </script>
 
 <style>
-	.user {
-		color: #fff;
-	}
+  .user {
+    color: #fff;
+  }
 
-	.credits {
-		font-size: .75rem;
-		color: #fff;
-	}
+  .credits {
+    font-size: 0.75rem;
+    color: #fff;
+  }
 
-	.credits a {
-		color: #fff;
-	}
+  .credits a {
+    color: #fff;
+  }
 
-	.navbar-brand {
-		display: block;
-	}
+  .navbar-brand {
+    display: block;
+  }
 
-	@media (min-width: 1024px) {
-		.navbar-brand {
-			display: flex
-		}
+  @media (min-width: 1024px) {
+    .navbar-brand {
+      display: flex;
+    }
 
-		.credits {
-			border-left: 1px solid #fff;
-		}
-	}
+    .credits {
+      border-left: 1px solid #fff;
+    }
+  }
 </style>
 
-  <nav class="navbar is-info">
-		<div class="navbar-brand">
-			<div class="navbar-item">
-				Spotify Stats
-			</div>
-			<div class="navbar-item">
-				{#if user}<SelectPlaylist on:playlistSelected={e => playlistId = e.detail} />{/if}
-			</div>
-		</div>
-		
-    <div class="navbar-end">
-			<div class="navbar-item">
-					{#await initUserPromise}
-						<p>Loading...</p>
-					{:then user}
-						{#if user}
-							<p class="user">{user.displayName}</p>
-						{/if}
-					{:catch error}
-						<p>Failed to get user</p>
-					{/await}
-			</div>
-			<div class="navbar-item credits">
-				<p>
-				Built using <a href="https://svelte.dev/">Svelte</a><br>
-				<a href="https://github.com/dhulme/spotify-stats">github.com/dhulme/spotify-stats</a>
-				</p>
-			</div>
+<nav class="navbar is-info">
+  <div class="navbar-brand">
+    <div class="navbar-item">Spotify Stats</div>
+    <div class="navbar-item">
+      {#if user}
+        <SelectPlaylist on:playlistSelected={e => (playlistId = e.detail)} />
+      {/if}
     </div>
-  </nav>
+  </div>
 
-	<section class="section">
-	{#if user}
-		<TrackFetcher {playlistId} on:tracksChanged={e => tracks = e.detail} />
-		{#if tracks.length}
-			<ChartTracks {tracks} />
-			<ChartArtist {tracks} />
-			<ChartTopArtists {tracks} />
-		{/if}
-	{/if}
-	</section>
+  <div class="navbar-end">
+    <div class="navbar-item">
+      {#await initUserPromise}
+        <p>Loading...</p>
+      {:then user}
+        {#if user}
+          <p class="user">{user.displayName}</p>
+        {/if}
+      {:catch error}
+        <p>Failed to get user</p>
+      {/await}
+    </div>
+    <div class="navbar-item credits">
+      <p>
+        Built using
+        <a href="https://svelte.dev/">Svelte</a>
+        <br />
+        <a href="https://github.com/dhulme/spotify-stats">
+          github.com/dhulme/spotify-stats
+        </a>
+      </p>
+    </div>
+  </div>
+</nav>
+
+<section class="section">
+  {#if user}
+    <TrackFetcher {playlistId} on:tracksChanged={e => (tracks = e.detail)} />
+    {#if tracks.length}
+      <ChartTracks {tracks} />
+      <ChartArtist {tracks} />
+      <ChartTopArtists {tracks} />
+    {/if}
+  {/if}
+</section>
